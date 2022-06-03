c6fd1bc4ae45918678949215a05d86b52035c723
gims-inc <gabrielmusanga@gmail.com>
2022-06-03 14:55:08 -0700

    Lower and Upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Print the aplhabet in lowercase and uppercase using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet_char = 'a';
+while (alphabet_char <= 'z')
+{
+	putchar(alphabet_char);
+	alphabet_char++;
+}
+
+char alphabet_upper = 'A';
+while (alphabet_upper <= 'Z')
+{
+	putchar(alphabet_upper)
+	alphabet_upper++;
+}
+putchar('\n');
+return (0);
+}
