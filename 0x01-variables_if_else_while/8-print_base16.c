ec5bd624728a477b651c5d303b13818f67343289
gims-inc <gabrielmusanga@gmail.com>
2022-06-03 17:25:12 -0700

    Hexadecimal-base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print the 0 to 9 and a to f.
+ * use only 3 putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+
+for (number = 0; number < 10; number++)
+{
+	putchar(number + '0')
+}
+
+char alphabet_char = 'a';
+
+while (alphabet_char <= 'f')
+{
+	putchar(alphabet_char);
+	alphabet_char++;
+}
+putchar('\n');
+return (0);
+}
