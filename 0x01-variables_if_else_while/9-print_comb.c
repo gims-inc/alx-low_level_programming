caf53d93751e68c609838e8a2b632f48e1b866b8
gims-inc <gabrielmusanga@gmail.com>
2022-06-03 17:59:30 -0700

    Patience Persistence Perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single digits
+ * Ascending order
+ * only use putchar max 4
+ * do not use char
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+	putchar(i + '0');
+	if (i < 9)
+	{
+	putchar(44);/*comma*/
+	putchar(32);/*space*/
+	}
+	number++;
+	}
+	putchar('\n');
+	return (0);
+}
