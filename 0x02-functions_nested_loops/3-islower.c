541da6b2b7a8ba7796d0eb78b3aa0fa3c6da7c07
gims-inc <gabrielmusanga@gmail.com>
2022-06-07 13:03:07 -0700

    IsLower fix1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isLower - check for lowercase characters
+ * _islower - check for lowercase characters
  * return 1 if true and 0 if false.
  * @ch: char type
  * Return: 1 (true), 0 (false)
  */
-int _isLower(char ch)
+int _islower(char ch)
 {
 	if (ch >= 97 && ch <= 122)
 	{
