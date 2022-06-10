c3f6692984e259d21b0b2a6f60b263339aa3b041
gims-inc <gabrielmusanga@gmail.com>
2022-06-10 06:25:34 -0700

    Phew

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,9 +4,46 @@
  * print_number - prints an integer.
  * @n: int type number
  * Allowed  _putchar
- * Dissallowed (long)
+ * Dissallowed (long)int m;
  */
 void print_number(int n)
 {
+	int m;
+	int a, b, c, d;
 
+	if (n < 0)
+	{
+		m = n * -1;
+		_putchar(45);
+	}
+	else
+		m = n;
+
+	if (m >= 1000 && m < 10000)
+	{
+		a = m / 1000;
+		b = (m / 100) % 10;
+		c = (m / 10) % 10;
+		d = m % 10;
+	}
+	else if (m >= 100 && m < 1000)
+	{
+		a = m / 100;
+		b = (m / 10) % 10;
+		d = m % 10;
+	}
+	else if (m >= 10 && m < 100)
+	{
+		a = m / 10;
+		b = m % 10;
+	}
+	else if (m > -1 && m < 10)
+	{
+		a = m % 10;
+	}
+
+	a && _putchar(a + '0');
+	b && _putchar(b + '0');
+	c && _putchar(c + '0');
+	d && _putchar(d + '0');
 }
