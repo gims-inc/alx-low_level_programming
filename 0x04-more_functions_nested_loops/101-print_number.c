5867bb52df9e85d7acdbfafe17568bef77e9bf42
gims-inc <gabrielmusanga@gmail.com>
2022-06-14 01:33:47 -0700

    Fix -

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: int type number
- * Allowed  _putchar
- * Dissallowed (long)int m;
+ * print_number - prints an integer
+ *  @n: input integer
+ *  Return: no return
  */
 void print_number(int n)
 {
-	int m;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		m = n * -1;
 		_putchar(45);
+		m = n * -1;
 	}
 	else
+	{
 		m = n;
-
-	if (m >= 1000 && m < 10000)
-	{
-		_putchar(m / 1000 + '0');
-		_putchar((m / 100) % 10 + '0');
-		_putchar((m / 10) % 10 + '0');
-		_putchar(m % 10 + '0');
-	}
-	else if (m >= 100 && m < 1000)
-	{
-		_putchar(m / 100 + '0');
-		_putchar((m / 10) % 10 + '0');
-		_putchar(m % 10 + '0');
-	}
-	else if (m >= 10 && m < 100)
-	{
-		_putchar((m / 10) + '0');
-		_putchar(m % 10 + '0');
-	}
-	else if (m > -1 && m < 10)
-	{
-		_putchar(m % 10 + '0');
 	}
 
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
