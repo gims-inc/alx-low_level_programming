3bd2eca98fc62cb8b36f7601e59d8c6b6d21c221
gims-inc <gabrielmusanga@gmail.com>
2022-06-15 14:44:01 -0700

    Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string pointer
+ * @s2: string pointer
+ * Return: 0 if matchs,negative int if s1 < s2,poitive int if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	/**
+	 * int len;
+	 */
+
+	for (; *s1 == *s2; s1++, s2++)
+	{
+		if (*s1 != '\0')
+			return (0);
+	}
+	return (*s1 - *s2);
+}
