4497f6be50217553aa9d5b8015c97c8e702d9112
gims-inc <gabrielmusanga@gmail.com>
2022-06-16 01:43:31 -0700

    Compare strings fix2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 == *s2; s1++, s2++)
+	int len;
+
+	for (len = 0; s1[len] != '\0' || s2[len] != '\0'; len++)
 	{
-		if (*s1 != '\0')
-			return (0);
+		if (s1[len] != s2[len])
+			return (s1[len] - s2[len]);
 	}
-	return (*s1 - *s2);
+	return (0);
 }
