9fe017a6f24f2c305e76ecf0b89d0676eff1235a
gims-inc <gabrielmusanga@gmail.com>
2022-06-16 02:30:57 -0700

    Copy string fix1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int len;
 
-	for (len = 0; len < n && src[len]; len++)
+	for (len = 0; len < n && src[len] != '\0'; len++)
 		dest[len] = src[len];
 
 	for (; n > len; len++)
