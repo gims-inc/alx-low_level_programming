bd24b97662f59c72f8ffbf1622f2820a46a96bd4
gims-inc <gabrielmusanga@gmail.com>
2022-06-20 14:48:10 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any sets of bytes
+ * @s: pointer to string 1
+ * @accept: pointer to string 2
+ * Return: pointer to s byte if matching accept, Null if byte not found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int a, b;
+
+	for (a = 0; *(s + 1) != '\0'; a++)
+	{
+		for (b = 0; *(accept + b) != '\0'; b++)
+		{
+			if (*(s + a) == *(accept + b))
+					return (s + a);
+		}
+	}
+	return ('\0');
+}
