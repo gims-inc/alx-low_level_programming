827e80ee0311de445d943eb3e67e3d6ed28dc639
gims-inc <gabrielmusanga@gmail.com>
2022-06-20 15:58:54 -0700

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of apointer to a char
+ * @s: double pointer to char
+ * @to: pointer to char
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
