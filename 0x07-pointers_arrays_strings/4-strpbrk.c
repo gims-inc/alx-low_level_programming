4e23afb8c207135a8cc7b5f0b9b125c721549560
gims-inc <gabrielmusanga@gmail.com>
2022-06-20 15:45:07 -0700

    strpbrk fix1

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,13 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int a, b;
 
-	for (a = 0; *(s + 1) != '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		for (b = 0; *(accept + b) != '\0'; b++)
+		for (b = 0; accept[b] != '\0'; b++)
 		{
-			if (*(s + a) == *(accept + b))
-					return (s + a);
+			if (s[a] == accept[b])
+				return (s + a);
 		}
 	}
-	return ('\0');
+	return (0);
 }
