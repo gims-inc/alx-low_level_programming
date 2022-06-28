5c7b1f36be3cf997f77f69c0c3a2406298f4a726
gims-inc <gabrielmusanga@gmail.com>
2022-06-28 13:32:04 -0700

    Concat fix4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	int strLen1, strLen2, count, count1;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 
 
 	for (strLen1 = 0; s1[strLen1] != '\0'; strLen1++)
@@ -24,12 +24,12 @@ char *str_concat(char *s1, char *s2)
 	for (strLen2 = 0; s2[strLen2] != '\0'; strLen2++)
 		;
 
-	ptr = malloc((strLen1 * sizeof(*s1)) + (strLen2 * sizeof(*s1)) + 1);
+	ptr = malloc((strLen1 * sizeof(*s1)) + (strLen2 * sizeof(*s2)) + 1);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (count = 0, count1 = 0; count < (strLen1 + strLen2); count++)
+	for (count = 0, count1 = 0; count < (strLen1 + strLen2 + 1); count++)
 		if (count < strLen1)
 			ptr[count] = s1[count];
 		else
