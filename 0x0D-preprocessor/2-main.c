eec807ec878d4e9a9f0ee50822749c0c6028b8d6
gims-inc <gabrielmusanga@gmail.com>
2022-07-04 16:03:14 -0700

    FILE macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file where the program is compiled
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
