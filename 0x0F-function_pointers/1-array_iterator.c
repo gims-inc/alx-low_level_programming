8fcda6c72ab0d8829ff0c2627443f46224f9af6a
gims-inc <gabrielmusanga@gmail.com>
2022-07-06 13:18:01 -0700

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function tha executes function given as aa parameter
+ * @array - array pointer
+ * @size - size_t size of array
+ * @action - callback, function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t count;
+
+	for (count = 0; count < size; count++)
+			action(array[count]);
+}
