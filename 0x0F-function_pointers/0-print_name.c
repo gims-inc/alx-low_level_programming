ed8508b239c3366e1061256dd58b3c761c0187d8
gims-inc <gabrielmusanga@gmail.com>
2022-07-06 13:29:43 -0700

    Print name fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,12 @@
  * print_name - function that prints a name
  * @name: string name
  * @f: callback
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (array == NULL || f == NULL)
+		return;
 	f(name);
 }
