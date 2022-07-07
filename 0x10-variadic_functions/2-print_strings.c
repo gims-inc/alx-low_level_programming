a5465395ff981885c7525215567f7a5472129b2a
gims-inc <gabrielmusanga@gmail.com>
2022-07-07 16:38:29 -0700

    Print em strings fix4

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 		printf("%s", strPtr);
 
-		if (i != (n - 1) && seperator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 
 	}
