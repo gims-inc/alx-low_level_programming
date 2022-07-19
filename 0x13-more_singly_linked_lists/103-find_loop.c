afd2a025f4d1531df1c51c7f16716bca0ec6f68b
gims-inc <gabrielmusanga@gmail.com>
2022-07-19 06:57:40 -0700

    Find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find the loop in a linked list
+ * @head: head of linked list
+ * Description: Not allowed to use malloc, free or arrays.
+ * Can only declare a max of 2 variables.
+ *
+ * Return: Address of node where loop starts, or NULL if no loop found.
+ */
+
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (slow);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (NULL);
+}
