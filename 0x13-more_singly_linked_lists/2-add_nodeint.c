9e73ed49bf7d85a73a36b592f36e950816105695
gims-inc <gabrielmusanga@gmail.com>
2022-07-19 02:26:55 -0700

    Add node at begining fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,20 +10,16 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp;
 	listint_t *new_node;
 
-	temp = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
+	new_node->next = *head;
+
 	*head = new_node;
 
-	if (temp == NULL)
-		new_node->next = NULL;
-	else
-		new_node->next = temp;
-
-	return (temp);
+	return (new_node);
 }
