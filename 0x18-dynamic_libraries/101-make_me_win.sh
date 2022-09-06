1034c93a9da68b13299561023e37ea87eda88de7
gims-inc <gabrielmusanga@gmail.com>
2022-09-06 15:53:31 -0700

    Code injection --fix2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/gims-inc/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp/ https://github.com/gims-inc/alx-low_level_programming/blob/master/0x18-dynamic_libraries/winner.so
+export LD_PRELOAD=/tmp/winner.so
