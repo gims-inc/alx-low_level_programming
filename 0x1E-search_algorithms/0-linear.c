d31e3f4d923f7ed319f5fbac4c3cdc481ad44120
gims-inc <gabrielmusanga@gmail.com>
2023-01-05 14:20:02 +0300

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * linear_search -Searches fo a value in an array 
+ * linear_search -Searches fo a value in an array
  *  using Linear Search algorithm.
  * @array: pointer to the array
  * @size: -number of elements in the array
@@ -10,18 +10,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int counter;
-    unsigned int findex;
+	unsigned int counter;
+	unsigned int findex;
 
-    for (counter = 0; counter < size; counter++)
-    {
-        printf("Value checked array[%d] = [%d] \n", counter, array[counter]);
-        if (value == array[counter])
-        {
-            findex = counter;
-            if (findex)
-                return (findex);
-        }
-    }
-    return (-1);
+	for (counter = 0; counter < size; counter++)
+		{
+			printf("Value checked array[%d] = [%d] \n", counter, array[counter]);
+			if (value == array[counter])
+			{
+				findex = counter;
+				if (findex)
+					return (findex);
+			}
+		}
+	return (-1);
 }
