ee163871951d62217db5d9225bea3bb41c303eb0
gims-inc <gabrielmusanga@gmail.com>
2023-01-05 14:28:49 +0300

    --f2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,8 @@ int linear_search(int *array, size_t size, int value)
 	unsigned int counter;
 	unsigned int findex;
 
+	if (array == NULL)
+		return (-1);
 	for (counter = 0; counter < size; counter++)
 		{
 			printf("Value checked array[%d] = [%d] \n", counter, array[counter]);
