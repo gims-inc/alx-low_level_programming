36d7d39bd08acf1d7b5bd908a611fff25f7a0122
gims-inc <gabrielmusanga@gmail.com>
2023-01-05 18:46:26 +0300

    betty -inclussion

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,10 @@
-#ifndef _SEARCH_ALGOS_H
-#define _SERACH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
